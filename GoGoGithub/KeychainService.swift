03dbbc5b7f5536c16c00949875bf373594cf5f3a
matthew weintrub <heytrub@gmail.com>
2015-12-17 14:33:42 -0800

    adds KeychainService

---
+++ GoGoGithub/KeychainService.swift
@@ -0,0 +1,62 @@
+//
+//  KeyChainService.swift
+//  GoGoGithub
+//
+//  Created by Matthew Weintrub on 12/17/15.
+//  Copyright © 2015 matthew weintrub. All rights reserved.
+//
+
+import UIKit
+import Security
+
+// Identifiers
+let userAccount = "github"
+let accessGroup = "MyService"
+
+// Arguments for the keychain queries
+let kSecClassValue = kSecClass as NSString
+let kSecAttrAccountValue = kSecAttrAccount as NSString
+let kSecValueDataValue = kSecValueData as NSString
+let kSecClassGenericPasswordValue = kSecClassInternetPassword as NSString
+let kSecAttrServiceValue = kSecAttrService as NSString
+let kSecMatchLimitValue = kSecMatchLimit as NSString
+let kSecReturnDataValue = kSecReturnData as NSString
+let kSecMatchLimitOneValue = kSecMatchLimitOne as NSString
+
+class KeychainService: NSObject {
+    
+    class func save(data: NSString) {
+        let dataFromString: NSData = data.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!
+        
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, userAccount, dataFromString], forKeys: [kSecClassValue, kSecAttrAccountValue, kSecValueDataValue])
+        
+        // Delete any existing items
+        SecItemDelete(keychainQuery)
+        
+        // Add the new keychain item
+        let status: OSStatus = SecItemAdd(keychainQuery, nil)
+    }
+    
+    class func loadFromKeychain() -> NSString? {
+        // Instantiate a new default keychain query
+        // Tell the query to return a result
+        // Limit our results to one item
+        
+        let keychainQuery: NSMutableDictionary = NSMutableDictionary(objects: [kSecClassGenericPasswordValue, userAccount, kCFBooleanTrue, kSecMatchLimitOneValue], forKeys: [kSecClassValue, kSecAttrAccountValue, kSecReturnDataValue, kSecMatchLimitValue])
+        
+        var dataTypeRef :AnyObject?
+        
+        // Search for the keychain items
+        let status: OSStatus = SecItemCopyMatching(keychainQuery, &dataTypeRef)
+        
+        var contentsOfKeychain: NSString?
+        
+        if let retainedData = dataTypeRef as? NSData {
+            contentsOfKeychain = NSString(data: retainedData, encoding: NSUTF8StringEncoding)
+        } else {
+            print("Nothing was retrieved from the keychain. Status code \(status)")
+        }
+        //dataTypeRef?.release()
+        return contentsOfKeychain
+    }
+}
